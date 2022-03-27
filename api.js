13c13f5b617a68b8df975f990b8b85883b1da2af
Kevin Poppe <kevin.poppe93@gmail.com>
2022-03-27 10:27:02 +0200

    post messages

--- api.js
+++ api.js
@@ -7,7 +7,11 @@ var cors = require("cors");
 const bcrypt = require("bcrypt");
 
 app.use(bodyParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 const port = process.env.PORT || 3300;
 
